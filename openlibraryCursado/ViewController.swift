866e7efdc5c4a2572da92183b9ea6b10249ebe59
Arturo Rivas <arturo.rivas@icloud.com>
2016-04-12 10:26:13 +0200

    Pendiente de pasear los datos y mostrar, probar con ISBN 0141010584

--- openlibraryCursado/ViewController.swift
+++ openlibraryCursado/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var títuloLabel: UILabel!
+    @IBOutlet weak var autoresLabel: UILabel!
+    @IBOutlet weak var portadaImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pulsoBuscar(sender: AnyObject) {
+        let textField = sender as! UITextField
+        
+        let url = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textField.text!)
+        
+        do {
+            let respuesta = try String(contentsOfURL: url!, encoding: NSUTF8StringEncoding)
+            
+            print("Respuesta: \(respuesta)")
+            
+            if respuesta != "{}" {
+
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Hay problemas con la conexión a Internet. Inténtelo de nuevo más tarde.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+            
+        } catch {
+            let alert = UIAlertController(title: "Error", message: "Hay problemas con la conexión a Internet. Inténtelo de nuevo más tarde.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
 
